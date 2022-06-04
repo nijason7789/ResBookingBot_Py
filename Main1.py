38e6d3b120b7958f4731ecbac6516cd5323e06b2
nijason7789 <nijason7789@gmail.com>
2022-06-04 20:07:07 +0800

    Test Failed

--- Main1.py
+++ Main1.py
@@ -3,10 +3,13 @@ from bs4 import BeautifulSoup
 
 Name =  '謝昀霖'
 Phone = '0912292900'
-url = 'https://inline.app/booking/-LR7EWHI84Fq7C2AdeaE:inline-live-2a466/-LR7EWIKquuEfFn8Z-Lc'
+url = 'https://inline.app/booking/-LR7EWHI84Fq7C2AdeaE:inline-live-2a466?language=zh-tw'
+#https://inline.app/booking/-LR7EWHI84Fq7C2AdeaE:inline-live-2a466/-LR7EWIKquuEfFn8Z-Lc/
+#https://www.dcard.tw/f/pet
 #https://inline.app/booking/-LR7EWHI84Fq7C2AdeaE:inline-live-2a466?language=zh-tw&utm_source=fordummies&utm_medium=article&utm_campaign=FO2021michelin&utm_content=CHILLIESINE210816michelin
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36'}
 #偽裝自己是使用瀏覽器登入
+
 r = requests.get(url, headers=headers)
 
 print(r.status_code)
@@ -15,6 +18,11 @@ if r.status_code == requests.codes.ok:
   print("OK")
   #檢查是否成功登入
 
-my_data = {'key1': 'value1', 'key2': 'value2'}
+soup = BeautifulSoup(r.text,"html.parser")
+print(r.text)
+
+
+
+#my_data = {'key1': 'value1', 'key2': 'value2'}
 
 #r = requests.post(url=url , data = my_data, headers=headers)
